49c96594ecc5d798a6f22b18271547199ba961d6
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-04-21 12:44:34 -0300

    movienatica arreglado

--- randomizadordepeliculas/MovieNautica.py
+++ randomizadordepeliculas/MovieNautica.py
@@ -8,18 +8,28 @@ def choose_and_remove_movie():
     
     with open("peliculas.txt", "r") as archivo:
         movies = [line.rstrip() for line in archivo]
-    
-    print(movies)
+    print(" ")
+    print(" ")
+    print(">>>Peliculas de la lista<<<")
     
     if not movies:
         return "No hay más películas en la lista."
     
+    for pelicula in movies:
+        print(f"    >{pelicula}<")
+    print(" ")
+    print(" ")
     # Elegir una película al azar
     chosen_movie = random.choice(movies)
-    print(f"La película elegida al azar es: {chosen_movie}")
-    
+    print(f">>  La película elegida al azar es: {chosen_movie}")
+    print(" ")
+    print(" ")
     # Eliminar la película de la lista
     movies.remove(chosen_movie)
+    
+    with open("peliculas.txt", "w") as archivo:
+        for movie in movies:
+            archivo.write(f"{movie}\n")
     
     return f"La lista de películas ahora es: {movies}"
 
